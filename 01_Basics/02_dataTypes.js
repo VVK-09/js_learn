fc81e1404ae1d575a9821cd0a4b39e14fc2d55da
VVK-09 <vaishnav.connects@gmail.com>
2024-11-27 10:02:50 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using Node.js Not Browser
+
+console.log(3+3)
+
+
+// Number => 2 to power 53
+// BigInt
+// String - ""
+// Boolean - true/false
+// null - standalone value
+// undefined - 
+// Symbol - unique
+
+
+// Object
+let a=2
+console.log(typeof a);
+
+console.log(typeof null);
+console.log(typeof undefined);
+
