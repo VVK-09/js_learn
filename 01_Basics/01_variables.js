1981a6f0f6f0c4af144749ccc4f001716602b918
VVK-09 <vaishnav.connects@gmail.com>
2024-11-27 11:05:33 +0000

    Conversion

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const account_ID = 154213           //Can't be chnaged
+let account_email = "vk@gmail.com"
+var account_pass = "123456"
+account_city = "Jaipur"
+
+let account_state;
+// account_ID = 99 ----> this is not allowed
+// console.log(account_ID);
+
+account_email = "kubade@gmail.com"
+account_pass="9999"
+console.table([account_email,account_city,account_pass,account_state]);
---
+++ 01_Basics/03_conversion_operations.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all JS code as newer version
+
+let score1 = "8a0"
+let score2 = 100
+
+let set1=Number(score1)
+let set2=Number(score2)
+
+console.log(typeof set1);
+console.log
+console.log(Declar);
+
